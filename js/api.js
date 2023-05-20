714a4324e5cf92f2ff8c7a11bbf096efe16a4049
unknown <tylermiller1321@outlook.com>
2023-05-20 16:37:51 -0500

    setting up for api data fetch

---
+++ js/api.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const api_key = '9d7d85c0f78fa1ce1954533d0aaeb8e6';
+
+const imageBaseURL = 'https://image.tmdb.org/t/p/'
+
+// *** fetch data from a server using 'url' and passes the result in JSON data to a 'callback' function, along with an optional parameter if has 'optionalParam' ***
