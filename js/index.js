e8e0cec0f9c6d0255e219e5cca2c804ff342906a
unknown <tylermiller1321@outlook.com>
2023-06-06 16:37:21 -0500

    format

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
 //import all components and functions
 
 import { sidebar } from "./sidebar.js";
 import { api_key, imageBaseURL, fetchDataFromServer } from "./api.js";
-import { createMovieCard } from "./movie-card.js"
+import { createMovieCard } from "./movie-card.js";
 const pageContent = document.querySelector("[page-content]");
 
 sidebar();
@@ -12,83 +12,79 @@ sidebar();
 //HOME PAGE SECTIONS (top rated, upcoming movies, trending movies)
 
 const homePageSections = [
-    {
-        title: "Upcoming Movies",
-        path: "/movie/upcoming"
-    },
-    {
-        title: "Weekly Trending Movies",
-        path: "/trending/movie/week"
-    },
-    {
-        title: "Top Rated Movies",
-        path: "/movie/top_rated"
-    }
-]
-
-
+  {
+    title: "Upcoming Movies",
+    path: "/movie/upcoming",
+  },
+  {
+    title: "Top Trending Movies",
+    path: "/trending/movie/week",
+  },
+  {
+    title: "Top Rated Movies",
+    path: "/movie/top_rated",
+  },
+];
 
 //fetch all genres
 const genreList = {
+  //create genre string from genre_id e.g. [23,43] => "action, romance"
+  asString(genreIdList) {
+    let newGenreList = [];
 
-    //create genre string from genre_id e.g. [23,43] => "action, romance"
-    asString(genreIdList) {
-        let newGenreList = [];
-
-        for (const genreId of genreIdList) {
-            this[genreId] && newGenreList.push(this[genreId]); 
-            // this == genreList
-        }
-        return newGenreList.join(", ");
+    for (const genreId of genreIdList) {
+      this[genreId] && newGenreList.push(this[genreId]);
+      // this == genreList
     }
-
-
+    return newGenreList.join(", ");
+  },
 };
 
-fetchDataFromServer(`https://api.themoviedb.org/3/genre/movie/list?api_key=${api_key}`, function({ genres }) {
-    for (const { id, name } of genres){
-        genreList[id] = name;
+fetchDataFromServer(
+  `https://api.themoviedb.org/3/genre/movie/list?api_key=${api_key}`,
+  function ({ genres }) {
+    for (const { id, name } of genres) {
+      genreList[id] = name;
     }
 
-    fetchDataFromServer(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&page=1`, heroBanner);
-});
+    fetchDataFromServer(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&page=1`,
+      heroBanner
+    );
+  }
+);
 
+const heroBanner = function ({ results: movieList }) {
+  const banner = document.createElement("section");
+  banner.classList.add("banner");
+  banner.ariaLabel = "Popular Movies";
 
-
-
-const heroBanner = function({ results: movieList }){
-
-    const banner = document.createElement("section");
-    banner.classList.add("banner");
-    banner.ariaLabel = "Popular Movies";
-
-    banner.innerHTML = `
+  banner.innerHTML = `
     <div class="banner-slider"></div>
     <div class="slider-control">
         <div class="control-inner"></div>
     </div>
     `;
 
-    let controlItemIndex = 0;
+  let controlItemIndex = 0;
 
-    for (const [index, movie] of movieList.entries()){
+  for (const [index, movie] of movieList.entries()) {
+    const {
+      backdrop_path,
+      title,
+      release_date,
+      genre_ids,
+      overview,
+      poster_path,
+      vote_average,
+      id,
+    } = movie;
 
-        const {
-            backdrop_path,
-            title,
-            release_date,
-            genre_ids,
-            overview,
-            poster_path,
-            vote_average,
-            id
-        } = movie
+    const sliderItem = document.createElement("div");
+    sliderItem.classList.add("slider-item");
+    sliderItem.setAttribute("slider-item", "");
 
-        const sliderItem = document.createElement("div");
-        sliderItem.classList.add("slider-item");
-        sliderItem.setAttribute("slider-item", "");
-
-        sliderItem.innerHTML = `
+    sliderItem.innerHTML = `
         <img
             src="${imageBaseURL}w1280${backdrop_path}"
             alt="${title}"
@@ -122,15 +118,15 @@ const heroBanner = function({ results: movieList }){
         </div>
         `;
 
-        banner.querySelector(".banner-slider").appendChild(sliderItem);
+    banner.querySelector(".banner-slider").appendChild(sliderItem);
 
-        const controlItem = document.createElement("button");
-        controlItem.classList.add("poster-box", "slider-item");
-        controlItem.setAttribute("slider-control", `${controlItemIndex}`);
+    const controlItem = document.createElement("button");
+    controlItem.classList.add("poster-box", "slider-item");
+    controlItem.setAttribute("slider-control", `${controlItemIndex}`);
 
-        controlItemIndex++;
+    controlItemIndex++;
 
-        controlItem.innerHTML = `
+    controlItem.innerHTML = `
         <img
           src="${imageBaseURL}w154${poster_path}"
           alt="Slide to ${title}"
@@ -140,56 +136,57 @@ const heroBanner = function({ results: movieList }){
         />
         `;
 
-        banner.querySelector(".control-inner").appendChild(controlItem);
-    }
+    banner.querySelector(".control-inner").appendChild(controlItem);
+  }
 
-    pageContent.appendChild(banner);
+  pageContent.appendChild(banner);
 
-    addHeroSlide();
+  addHeroSlide();
 
-    // fetch data for homepage sections(top rated, upcoming, trending)
-    for (const { title, path } of homePageSections){
-        fetchDataFromServer(`https://api.themoviedb.org/3/${path}?api_key=${api_key}&page=1`, createMovieList, title)
-    }
-}
-
+  // fetch data for homepage sections(top rated, upcoming, trending)
+  for (const { title, path } of homePageSections) {
+    fetchDataFromServer(
+      `https://api.themoviedb.org/3/${path}?api_key=${api_key}&page=1`,
+      createMovieList,
+      title
+    );
+  }
+};
 
 //hero slider functionality
-const addHeroSlide = function() {
+const addHeroSlide = function () {
+  const sliderItems = document.querySelectorAll("[slider-item]");
+  const sliderControls = document.querySelectorAll("[slider-control]");
 
-    const sliderItems = document.querySelectorAll("[slider-item]");
-    const sliderControls = document.querySelectorAll("[slider-control]");
-    
-    let lastSliderItem = sliderItems[0];
-    let lastSliderControl = sliderControls[0];
+  let lastSliderItem = sliderItems[0];
+  let lastSliderControl = sliderControls[0];
 
-    lastSliderItem.classList.add("active");
-    lastSliderControl.classList.add("active");
+  lastSliderItem.classList.add("active");
+  lastSliderControl.classList.add("active");
 
-    const sliderStart = function () {
-        lastSliderItem.classList.remove("active");
-        lastSliderControl.classList.remove("active");
-    
-    
-        //this == slider-control
-        sliderItems[Number(this.getAttribute("slider-control"))].classList.add("active");
-        this.classList.add("active");
+  const sliderStart = function () {
+    lastSliderItem.classList.remove("active");
+    lastSliderControl.classList.remove("active");
 
-        lastSliderItem = sliderItems[Number(this.getAttribute("slider-control"))];
-        lastSliderControl = this;
-    }
+    //this == slider-control
+    sliderItems[Number(this.getAttribute("slider-control"))].classList.add(
+      "active"
+    );
+    this.classList.add("active");
 
-    addEventOnElements(sliderControls, "click", sliderStart);
+    lastSliderItem = sliderItems[Number(this.getAttribute("slider-control"))];
+    lastSliderControl = this;
+  };
 
-}
+  addEventOnElements(sliderControls, "click", sliderStart);
+};
 
 const createMovieList = function ({ results: movieList }, title) {
+  const movieListElem = document.createElement("section");
+  movieListElem.classList.add("movie-list");
+  movieListElem.ariaLabel = `${title}`;
 
-    const movieListElem = document.createElement("section");
-    movieListElem.classList.add("movie-list");
-    movieListElem.ariaLabel = `${title}`;
-  
-    movieListElem.innerHTML = `
+  movieListElem.innerHTML = `
       <div class="title-wrapper">
         <h3 class="title-large">${title}</h3>
       </div>
@@ -198,14 +195,13 @@ const createMovieList = function ({ results: movieList }, title) {
         <div class="slider-inner"></div>
       </div>
     `;
-  
-    for (const movie of movieList) {
-      const movieCard = createMovieCard(movie); // called from movie_card.js
-  
-      movieListElem.querySelector(".slider-inner").appendChild(movieCard);
-    }
-  
-    pageContent.appendChild(movieListElem);
-  
+
+  for (const movie of movieList) {
+    const movieCard = createMovieCard(movie); // called from movie_card.js
+
+    movieListElem.querySelector(".slider-inner").appendChild(movieCard);
   }
-  search();
+
+  pageContent.appendChild(movieListElem);
+};
+search();
